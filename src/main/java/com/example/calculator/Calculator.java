2c69fa6f14f1297dae5d566c134710b3450e5c15
Golisir <sivasankargoli126@gmail.com>
2025-10-17 10:40:17 +0530

    Update Calculator.java

--- src/main/java/com/example/calculator/Calculator.java
+++ src/main/java/com/example/calculator/Calculator.java
@@ -17,7 +17,9 @@ public class Calculator {
     public int divide(int a, int b) {
         if (b == 0) {
             throw new IllegalArgumentException("Division by zero");
-        }
-        return a / b;
+        } 
+        else {
+            return a / b;
     }
+  }
 }
